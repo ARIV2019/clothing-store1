f7595f39397429a6d50662f7bcedfdea585d7ef2
cdban7 <cdban7@yandex.ru>
2020-12-31 05:56:10 +0300

    сделаны миграции

--- geekshop/authapp/pipeline.py
+++ geekshop/authapp/pipeline.py
@@ -1,12 +1,13 @@
 from collections import OrderedDict
 from datetime import datetime
 from urllib.parse import urlencode, urlunparse
-
+import urllib.request
 import requests
+from django.conf import settings
 from django.utils import timezone
 from social_core.exceptions import AuthForbidden
 from authapp.models import ShopUserProfile
-
+import os
 
 def save_user_profile(backend, user, response, *args, **kwargs):
     if backend.name != 'vk-oauth2':
@@ -16,7 +17,7 @@ def save_user_profile(backend, user, response, *args, **kwargs):
                           'api.vk.com',
                           '/method/users.get',
                           None,
-                          urlencode(OrderedDict(fields=','.join(('bdate', 'sex', 'about')),
+                          urlencode(OrderedDict(fields=','.join(('bdate', 'sex', 'about', 'photo_400_orig')),
                                                 access_token=response['access_token'],
                                                 v='5.92')),
                           None
@@ -43,6 +44,12 @@ def save_user_profile(backend, user, response, *args, **kwargs):
         if age < 18:
             user.delete()
             raise AuthForbidden('social_core.backends.vk.VKOAuth2')
+    if data['photo_400_orig']:
+        urllib.request.urlretrieve(
+            data['photo_400_orig'],
+            os.path.join(settings.MEDIA_ROOT, 'users_avatars', f'{user.pk}.jpg')
+        )
+        user.avatar = os.path.join('users_avatars', f'{user.pk}.jpg')
 
     user.save()
 
