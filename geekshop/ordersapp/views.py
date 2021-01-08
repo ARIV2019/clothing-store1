ab5a91f4f37f862206bd5518dbf91e236d976a16
cdban7 <cdban7@yandex.ru>
2021-01-08 20:48:35 +0300

    Асинхронно загружаем цену продукта

--- geekshop/ordersapp/views.py
+++ geekshop/ordersapp/views.py
@@ -1,5 +1,6 @@
 from django.db.models.signals import pre_save, pre_delete
 from django.dispatch import receiver
+from django.http import JsonResponse
 from django.shortcuts import get_object_or_404, HttpResponseRedirect
 from django.urls import reverse, reverse_lazy
 from django.db import transaction
@@ -140,3 +141,11 @@ def product_quantity_update_delete(sender, instance, **kwargs):
     instance.product.quantity += instance.quantity
     instance.product.save()
 
+
+def get_product_price(request, pk):
+    if request.is_ajax():
+        product = Product.objects.filter(pk=int(pk)).first()
+        if product:
+            return JsonResponse({'price': product.price})
+        else:
+            return JsonResponse({'price': 0})
