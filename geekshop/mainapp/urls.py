c63c692e8fe17bd9cc83682c0c66663695b7375c
Igor <cdban7@yandex.ru>
2021-01-24 15:43:37 +0300

    urls

--- geekshop/mainapp/urls.py
+++ geekshop/mainapp/urls.py
@@ -18,12 +18,12 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 import mainapp.views as mainapp
-
+from django.views.decorators.cache import cache_page
 app_name = 'mainapp'
 
 urlpatterns = [
     path('', mainapp.products, name='index'),
-    path('category/<int:pk>', mainapp.products, name='category'),
+    path('category/<int:pk>', cache_page(600)(mainapp.products), name='category'),
     path('category/<int:pk>/<page>/', mainapp.products, name='page'),
     path('product/<int:pk>', mainapp.product, name='product'),
 ]
